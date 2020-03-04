540d24abec3a5e48aaf607d50dbba399136a08f1
Zachary Luther <Zluther89@gmail.com>
2020-03-04 12:35:33 -0500

    connected with db

--- db/sql/index.js
+++ db/sql/index.js
@@ -1,4 +1,8 @@
-const mysql = require('mysql');
-const mysqlConfig = require('../../config.js');
+const mysql = require("mysql");
+const mysqlConfig = require("../../config.js");
 
 const connection = mysql.createConnection(mysqlConfig);
+
+connection.query("SHOW TABLES", (err, data) => console.log(data));
+
+module.exports = connection;
--- db/sql/moviesSchema.sql
+++ db/sql/moviesSchema.sql
@@ -1,1 +1,12 @@
 -- SET UP SCHEMA HERE
+
+DROP DATABASE IF EXISTS badmovies;
+
+CREATE DATABASE badmovies;
+
+USE badmovies;
+
+CREATE TABLE favorites(
+    id integer auto_increment PRIMARY KEY,
+    moviename varchar(200)
+);
