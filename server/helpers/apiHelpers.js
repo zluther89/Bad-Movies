b4beee599aca15f70ac99414576d3b33c24aa139
Zachary Luther <Zluther89@gmail.com>
2020-03-04 12:06:01 -0500

    implemented get for all genres and specific movies by genre match

--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -5,18 +5,22 @@ const { API_KEY } = require("../../config.js");
 // write out logic/functions required to query TheMovieDB.org
 
 let searchWorstMoviesByGenreId = genreID => {
-  let options = {
-    //
-  };
-
   //makes an axios get request to the worst rated movies, by genre id
-  axios({
-    url: `https://api.themoviedb.org/3/discover/movie?api_key=d8c38afbc21d549e81fd17fbd3b8a1c2&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&with_genres=${genreID}`,
+
+  //NOTE: RETURN AXIOS FOR PROMISE
+  return axios({
+    url: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.asc&include_adult=false&include_video=false&page=1&with_genres=${genreID}`,
     method: "get"
-  }).then(res => console.log(res.data));
+  });
 };
 
-searchWorstMoviesByGenreId("28"); // A TEST CALL TO BE REMOVED
+let searchMovieGenres = () => {
+  return axios({
+    url: `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`,
+    method: "get"
+  });
+};
+
 // FOR REFERENCE:
 // https://www.themoviedb.org/account/signup
 // https://developers.themoviedb.org/3/discover/movie-discover
@@ -24,4 +28,7 @@ searchWorstMoviesByGenreId("28"); // A TEST CALL TO BE REMOVED
 
 // Don't forget to export your functions and require them within your server file
 
-module.exports = searchWorstMoviesByGenreId;
+module.exports = {
+  searchWorstMoviesByGenreId: searchWorstMoviesByGenreId,
+  searchMovieGenres: searchMovieGenres
+};
