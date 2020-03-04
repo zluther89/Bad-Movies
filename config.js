4cdd87acba06851cc4e1289c563ccde3b9eb1347
Zachary Luther <Zluther89@gmail.com>
2020-03-03 20:59:04 -0500

    started project

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'badmovies',
-  API_KEY: '',
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "badmovies",
+  API_KEY: "d8c38afbc21d549e81fd17fbd3b8a1c2"
 };
--- server/helpers/apiHelpers.js
+++ server/helpers/apiHelpers.js
@@ -1,8 +1,21 @@
-const request = require('request');
-const axios = require('axios');
-const { API_KEY } = require('../../config.js');
+const request = require("request");
+const axios = require("axios");
+const { API_KEY } = require("../../config.js");
 
 // write out logic/functions required to query TheMovieDB.org
+
+let searchMovies = function() {
+  let options = {
+    //
+  };
+  //how to make api request... key d8c38afbc21d549e81fd17fbd3b8a1c2
+
+  axios({
+    url: null, //some url,
+    method: "get",
+    headers: options
+  });
+};
 
 // FOR REFERENCE:
 // https://www.themoviedb.org/account/signup
@@ -10,3 +23,5 @@ const { API_KEY } = require('../../config.js');
 // Get your API Key and save it in your config file
 
 // Don't forget to export your functions and require them within your server file
+
+module.exports = searchMovies;
