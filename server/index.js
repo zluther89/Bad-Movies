71039734cddaed68b6fc83eee51bead9affcf91f
Zachary Luther <Zluther89@gmail.com>
2020-03-08 10:11:34 -0400

    some stuff

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var request = require("request");
 var movieDBController = require("./controllers/movieController");
 var app = express();
 
